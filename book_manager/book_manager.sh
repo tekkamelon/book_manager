5e4d7d8b73c3f6cc3a9eb8264d276e9ea268b2b9
tekkamelon <type2guntank@gmail.com>
2021-05-20 21:23:58 +0900

    各種修正
     curlから取得したデータを"/tmp"ディレクトリに一時保存

--- book_manager/book_manager.sh
+++ book_manager/book_manager.sh
@@ -17,17 +17,19 @@ do
 		# json形式を整形して出力
 		json_format=$(echo "\&pretty")
 			
+			# openBDのapiからデータを取得,"/tmp/data.tmp"に一時保存
+			curl -s https://api.openbd.jp/v1/get?isbn=$isbn$json_format > /tmp/data.tmp
 			# タイトル
-			title=$(curl -s https://api.openbd.jp/v1/get?isbn=$isbn$json_format | grep title | sed 's/^.*"\(.*\)".*$/\1/' | grep -v "Subtitle" | sed 's/,//g')
+			title=$(cat /tmp/data.tmp | grep title | sed 's/^.*"\(.*\)".*$/\1/' | grep -v "Subtitle" | sed 's/,//g') && 
 		
 			# 著者名
-			author=$(curl -s https://api.openbd.jp/v1/get?isbn=$isbn$json_format | grep author | sed 's/^.*"\(.*\)".*$/\1/' | grep -v "Subtitle" | sed 's/,//g')
+			author=$(cat /tmp/data.tmp | grep author | sed 's/^.*"\(.*\)".*$/\1/' | grep -v "Subtitle" | sed 's/,//g') && 
 
 			# 出版社
-			publisher=$(curl -s https://api.openbd.jp/v1/get?isbn=$isbn$json_format | grep publisher | sed 's/^.*"\(.*\)".*$/\1/' | grep -v "Subtitle" | sed 's/,//g')
+			publisher=$(cat /tmp/data.tmp | grep publisher | sed 's/^.*"\(.*\)".*$/\1/' | grep -v "Subtitle" | sed 's/,//g') &&
 
 			# 発売日
-			pubdate=$(curl -s https://api.openbd.jp/v1/get?isbn=$isbn$json_format | grep pubdate | sed 's/^.*"\(.*\)".*$/\1/' | grep -v "Subtitle" | sed 's/,//g')
+			pubdate=$(cat /tmp/data.tmp | grep pubdate | sed 's/^.*"\(.*\)".*$/\1/' | grep -v "Subtitle" | sed 's/,//g') &&
 
 		# csvファイルに取得した内容を追記,1行目および末尾3行目を表示
 		echo "$isbn,$title,$author,$publisher,$pubdate" >> $file ; column -t -s, $file | sed -n '1p' && tail -n -3 $file | column -t -s,
