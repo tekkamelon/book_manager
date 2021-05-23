bb11ea0fda607be5dd614c7bf2339a332291d9ac
tekkamelon <type2guntank@gmail.com>
2021-05-24 02:56:43 +0900

    各種修正
    テータに"Q"が含まれると保存されないバグの修正,保存先のディレクトリの変更など

--- book_manager/book_manager.sh
+++ book_manager/book_manager.sh
@@ -3,10 +3,10 @@
 while :
 do
 	# 保存先のディレクトリ,ファイル名
-	file=/tmp/library.csv
+	file=~/Documents/library/library.csv
 
 	if [ "$isbn" = "Q" ]; then
-		echo "終了します" && sed -i '/Q/d' $file
+		echo "入力を終了" 
 		break
 
 	else
@@ -17,9 +17,8 @@ do
 		# json形式を整形して出力
 		json_format=$(echo "\&pretty")
 
-		echo -n -e "\n" ;
 		# openBDからデータを取得,isbn,タイトル,出版社,発売日,著者を取得し,カンマ区切りにして追記
-		curl -s https://api.openbd.jp/v1/get?isbn=$isbn$json_format | grep -e isbn -e title -e author -e publisher -e pubdate | sed 's/^.*"\(.*\)".*$/\1/' | grep -v "Subtitle" | sed -z -e 's/\n/,/g' -e "s/,\$/\n/" >> $file ; cat $file | column -t -s, | sed -n '1p' && tail -n -3 $file | column -t -s, && echo -n -e "\n" 
+		echo -n -e "\n" && curl -s https://api.openbd.jp/v1/get?isbn=$isbn$json_format | grep -e isbn -e title -e publisher -e pubdate -e author | sed 's/^.*"\(.*\)".*$/\1/' | grep -v "Subtitle" | sed -z -e 's/\n/,/g' -e "s/,\$/\n/" | tee -a $file | column -t -s, && echo -n -e "\n"
 
 	fi
 done
