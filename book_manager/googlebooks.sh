a034ba43407e0c14307a9949da93478ccdd51f1a
tekkamelon <type2guntank@gmail.com>
2021-06-06 16:13:01 +0900

    ループの実装
    googlebooks.shにwhile文によるループを実装

--- book_manager/googlebooks.sh
+++ book_manager/googlebooks.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
-tmpfile=/tmp/gbtmp.json
+while :
+do
+	# 保存先のディレクトリ,ファイル名
+	file=~/Documents/library/library.csv
 
-read -p "google books@ISBN > " "isbn"
+	# 一時保存先のディレクトリ,ファイル名
+	tmpfile=/tmp/gbtmp.json
 
-curl -s https://www.googleapis.com/books/v1/volumes?q=isbn:$isbn > $tmpfile && cat $tmpfile | jq '.items[0] | .volumeInfo | .authors ' | grep -e '\"' | sed 's/"//g'
+	if [ "$isbn" = "Q" ]; then
+		echo "入力を終了" 
+		break
+
+	else
+
+		# ISBN
+		read -p "google books@ISBN > " "isbn"
+
+		# google book apiからデータを取得
+		curl -s https://www.googleapis.com/books/v1/volumes?q=isbn:$isbn > $tmpfile && cat $tmpfile | jq '.items[0] | .volumeInfo | .authors ' | grep -e '\"' | sed 's/"//g'
+
+	fi
+done
