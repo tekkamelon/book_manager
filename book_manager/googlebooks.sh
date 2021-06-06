0cb735a1464443d40853075cc4cdb2b47c91718b
tekkamelon <type2guntank@gmail.com>
2021-06-06 18:11:44 +0900

    タイトル,発売日,著者名の追加
    タイトル,発売日,著者名の追加

--- book_manager/googlebooks.sh
+++ book_manager/googlebooks.sh
@@ -3,7 +3,8 @@
 while :
 do
 	# 保存先のディレクトリ,ファイル名
-	file=~/Documents/library/library.csv
+	# file=~/Documents/library/library.csv
+	file=/tmp/library.csv
 
 	# 一時保存先のディレクトリ,ファイル名
 	tmpfile=/tmp/gbtmp.json
@@ -17,8 +18,21 @@ do
 		# ISBN
 		read -p "google books@ISBN > " "isbn"
 
-		# google book apiからデータを取得
-		curl -s https://www.googleapis.com/books/v1/volumes?q=isbn:$isbn > $tmpfile && cat $tmpfile | jq '.items[0] | .volumeInfo | .authors ' | grep -e '\"' | sed 's/"//g'
+		# google book api よりデータを取得
+		curl -s https://www.googleapis.com/books/v1/volumes?q=isbn:$isbn > $tmpfile 
+
+		# タイトル
+		title=$(cat $tmpfile | grep title | sed 's/^.*"\(.*\)".*$/\1/' | grep -v "Subtitle" | sed 's/,//g') && 
+		# 出版社
+		publisher=$(cat $tmpfile | grep publisher | sed 's/^.*"\(.*\)".*$/\1/' | grep -v "Subtitle" | sed 's/,//g') && 
+
+		# 発売日 
+		pubdate=$(cat $tmpfile | grep publishedDate | sed 's/^.*"\(.*\)".*$/\1/' | grep -v "Subtitle" | sed 's/,//g') && 
+
+		# 著者名
+		author=$(cat $tmpfile | jq '.items[0] | .volumeInfo | .authors ' | grep -e '\"' | sed 's/"//g' | sed 's/,//g') && 
+
+		echo -n -e "\n" && echo "$title,$author,$publisher,$pubdate" >> $file ; column -t -s, $file | sed -n '1p' && tail -n -3 $file | column -t -s, && echo -n -e "\n"
 
 	fi
 done
