dcd6a63fb1f0a7aed0621041ca4667f974546656
tekkamelon <type2guntank@gmail.com>
2021-07-13 02:22:35 +0900

    軽微な修正
    grepでの"Subtitle"の除外方法を変更

--- book_manager/book_manager.sh
+++ book_manager/book_manager.sh
@@ -15,7 +15,7 @@ do
 		read -p "openBD@ISBN > " "isbn"
 
 		# openBDからデータを取得,isbn,タイトル,出版社,発売日,著者を取得し,カンマ区切りにして追記
-		echo -ne "\n" && echo "'https://api.openbd.jp/v1/get?isbn=$isbn&pretty'" | xargs curl -s | grep -e isbn -e title -e publisher -e pubdate -e author | grep -v "Subtitle" | sed 's/^.*"\(.*\)".*$/\1/' | perl -pe 's/\n/,/g' | perl -pe "s/,\$/\n/" | tee -a $file | column -ts, && echo -ne "\n"
+		echo -ne "\n" && echo "'https://api.openbd.jp/v1/get?isbn=$isbn&pretty'" | xargs curl -s | grep -e isbn -we title -e publisher -e pubdate -e author | sed 's/^.*"\(.*\)".*$/\1/' | perl -pe 's/\n/,/g' | perl -pe "s/,\$/\n/" | tee -a $file | column -ts, && echo -ne "\n"
 
 	fi
 done
