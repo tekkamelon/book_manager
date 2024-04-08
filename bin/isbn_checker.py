2aa7827dbec60a3cb2f04c8c24d45738cc7a716d
tekkamelon <type2guntank@gmail.com>
2024-04-08 16:41:40 +0900

    軽微な修正
    リストからタプルへ変更

--- bin/isbn_checker.py
+++ bin/isbn_checker.py
@@ -6,17 +6,17 @@ import sys
 input = sys.stdin.readline()
 input = input.replace('\n', '')
 
-# "input"をリスト化
-num_list = list(input)
+# "input"をタプル化
+num_tuple = tuple(input)
 
-# "num_list"をint型にキャスト,"num_list_int"に代入
-num_list_int = [int(i) for i in num_list]
+# "num_tuple"をint型にキャスト,"num_tuple_int"に代入
+num_tuple_int = [int(i) for i in num_tuple]
 
 # 奇数桁の合計
-odd_num = sum(num_list_int[0::2])
+odd_num = sum(num_tuple_int[0::2])
 
 # 偶数桁の合計の3倍
-even_num = sum(num_list_int[1::2] * 3)
+even_num = sum(num_tuple_int[1::2] * 3)
 
 # 奇数桁と偶数桁の3倍を合計
 addition = odd_num + even_num
